e17fe51954cd52f9bdbcc8a84fbb46f73304ee77
Kirill <skrollfase@mail.ru>
2025-06-01 15:17:22 +0700

    field display during game over

--- main.py
+++ main.py
@@ -13,9 +13,11 @@ def main():
 
         if make_move(status, x, y):
             if check_win(status["board"]):
+                show_board(status["board"])
                 print(f'Игрок {status["current_player"]} победил!')
                 status["game_over"] = True
             elif is_draw(status["board"]):
+                show_board(status["board"])
                 print("Ничья!")
                 status["game_over"] = True
             status["current_player"] = "X" if  status["current_player"] == "O" else "O"
