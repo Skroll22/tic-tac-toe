c1a221fdc2e009277c334bad119078a0bfdbcd8c
Kirill <skrollfase@mail.ru>
2025-06-01 15:14:53 +0700

    main.py has been written

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from display import show_board
+from game_logic import make_move, check_win, is_draw
 from status import init_status
 from status_handler import get_player_move
 
@@ -8,5 +9,16 @@ def main():
 
     while not status["game_over"]:
         show_board(status["board"])
-
         x, y = get_player_move()
+
+        if make_move(status, x, y):
+            if check_win(status["board"]):
+                print(f'Игрок {status["current_player"]} победил!')
+                status["game_over"] = True
+            elif is_draw(status["board"]):
+                print("Ничья!")
+                status["game_over"] = True
+            status["current_player"] = "X" if  status["current_player"] == "O" else "O"
+
+if __name__ == "__main__":
+    main()
